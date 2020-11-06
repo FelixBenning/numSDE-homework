4ed3b7b8dcbfd6a881ee4c86cb90962ba751389f
Felix Benning <felix.benning@gmail.com>
2020-11-06 19:47:42 +0100

    expected value calculation

--- ornsteinUhlbeck.m
+++ ornsteinUhlbeck.m
@@ -1,25 +1,26 @@
 function simulation = ornsteinUhlenbeck(T, x_0, mu, lambda, n)
     times = 0:T/n:T;
-    cov_matrix = covariance(times, lambda);
+    [expected_value, cov_matrix] = distribution_parameters(times, lambda, mu, x_0);
     A = chol(cov_matrix);
-    simulation = expectedValue(times, lambda) + rand(1, length(times)) * A;
+    simulation = expected_value + rand(1, length(times)) * A;
 end
 
-function cov_matrix = covaraince(times, lambda)
+function [expected_value, cov_matrix] = distribution_parameters(times, lambda, mu, x_0)
     % times: row vector of times (1xT)
     % return: TxT covariance matrix corresponding to times
     
     exp_vec = exp(-lambda .* times);
+    
+    % cov matrix calculation
     cov_matrix = transpose(exp_vec) * exp_vec ./(2*lambda);
     
-    temp = repelem( transpose(exp(2*lambda .* times)),1, length(times));
+    temp = repelem( transpose(exp(2*lambda .* times)), 1, length(times));
     min_matrix = triu(temp) + transpose(triu(temp,1)) - 1;
     
     cov_matrix = cov_matrix .* min_matrix;
-end
-
-function expectation = expectedValue(times, lambda)
-    % times: row vector of times
-    % return: row vector of the expected value corresponding to times
+    
+    % expected_value calculation
+    
+    expected_value = x_0 .* exp_vec + (mu .*(1 - exp_vec));
     
 end
